cc2725bfa4ddc57afcd60be6ddcac859ea9f18c2
armand.foucault@telecom-bretagne.eu <armand@armand-laptop.localhost>
2018-02-06 00:26:46 +0100

    Splitted Tileset into Pygame and Tkinter versions

--- src/core/tileset.py
+++ src/core/tileset.py
@@ -1,14 +1,34 @@
 import pygame
+from PIL import Image, ImageTk
 from . import picklable
 
 
 class Tileset(picklable.Picklable):
-    def __init__(self, image: pygame.Surface, tileSize: int):
+    def __init__(self, image, tileSize):
         self.image = image
         self.tileSize = tileSize
         self.width, self.height = self.image.get_size()
         self.columnsCount = self.width / self.tileSize
 
+    def __getitem__(self, index):
+        return self._get_tile(index)
+
+
+class TkTileset(Tileset):
+    def _get_tile(self, index):
+        row = index % self.columnsCount
+        column = index // self.columnsCount
+
+        rectangle = (
+            row * self.tileSize,
+            column * self.tileSize,
+            (row + 1) * self.tileSize,
+            (column + 1) * self.tileSize)
+        tile = self.image.crop(rectangle)
+        return ImageTk.PhotoImage(tile)
+
+
+class PygameTileset(Tileset):
     def _get_tile(self, index):
         row = index % self.columnsCount
         column = index // self.columnsCount
@@ -18,6 +38,3 @@ class Tileset(picklable.Picklable):
         tile.blit(self.image, rectangle)
 
         return tile.convert_alpha()
-
-    def __getitem__(self, index):
-        return self._get_tile(index)
