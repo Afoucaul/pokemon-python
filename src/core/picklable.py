02e3a97ee2d80378d77df29c5d950ae520b80b64
armand.foucault@telecom-bretagne.eu <armand@armand-laptop.localhost>
2018-02-07 15:12:36 +0100

    Added a PicklableOptions option enum

--- src/core/picklable.py
+++ src/core/picklable.py
@@ -1,18 +1,35 @@
 import pickle
 
+from .utils import BinaryEnum
+
+
+class PicklableError(Exception):
+    pass
+
+
+class PicklableOptions(BinaryEnum):
+    READONLY = 1
+
 
 class Picklable:
-    def __init__(self):
-        self.picklePath = None
+    def __init__(self, picklePath, options):
+        """Attributes defined by Picklable.load"""
+        self.picklePath = picklePath
+        self.options = options
 
     @classmethod
-    def load(cls, path):
+    def load(cls, path, options=0):
         with open(path, 'rb') as source:
             obj = pickle.load(source)
-            obj.picklePath = path
+            obj.__init__(path, options)
             return obj
 
     def dump(self, path=None):
+        options = PicklableOptions.get_options(self.options)
+        if PicklableOptions.READONLY in options:
+            raise PicklableError("Cannot dump read-only instance of {}".format(
+                type(self).__name__))
+
         if path is None:
             path = self.picklePath
 
