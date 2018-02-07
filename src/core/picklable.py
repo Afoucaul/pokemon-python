7e23fddbadb9871fa666c25942bd28d160733299
armand.foucault@telecom-bretagne.eu <armand@armand-laptop.localhost>
2018-02-07 01:11:08 +0100

    Removed PicklableError

--- src/core/picklable.py
+++ src/core/picklable.py
@@ -1,10 +1,6 @@
 import pickle
 
 
-class PicklableError(Exception):
-    pass
-
-
 class Picklable:
     def __init__(self):
         self.picklePath = None
@@ -13,19 +9,12 @@ class Picklable:
     def load(cls, path):
         with open(path, 'rb') as source:
             obj = pickle.load(source)
-
-        if isinstance(obj, cls):
             obj.picklePath = path
             return obj
-        else:
-            raise PicklableError("Tried to unpickle a {} instance,"
-                                 "but found a {} instance".format(
-                                     cls.__name__,
-                                     type(obj).__name__))
 
     def dump(self, path=None):
         if path is None:
             path = self.picklePath
 
         with open(path, 'wb') as target:
-            pickle.dump(target, self)
+            pickle.dump(self, target)
