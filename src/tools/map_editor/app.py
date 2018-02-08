ebd97b46114744fd5b7472154b91e193fb437165
armand.foucault@telecom-bretagne.eu <armand@armand-laptop.localhost>
2018-02-08 23:39:04 +0100

    Wrote file management

--- src/tools/map_editor/app.py
+++ src/tools/map_editor/app.py
@@ -1,12 +1,13 @@
 import tkinter as tk
 
 from core.overworld import Overworld
-from core.tileset import Tileset
+from core.picklable import PicklableOptions
 
-if __name__ == '__main__':
-    import map_frame
-else:
-    from . import map_frame
+import map_frame
+import tileset_frame
+import main_menu
+import collision_frame
+from tileset import TkTileset
 
 
 class SingletonError(Exception):
@@ -17,25 +18,52 @@ class App(tk.Tk):
     instance = None
 
     def __init__(self):
-        if self.instance is not None:
+        if type(self).instance is not None:
             raise SingletonError
         else:
-            self.instance = self
+            type(self).instance = self
+
+        super().__init__()
 
         self.tileset = None
         self.overworld = None
 
         self.mapFrame = map_frame.MapFrame(self)
-        self.mapFrame.pack()
+        self.tilesetFrame = tileset_frame.TilesetFrame(self)
+        self.mainMenu = main_menu.MainMenu(self)
+        self.collisionFrame = collision_frame.CollisionFrame(self)
+
+        self.mapFrame.pack(fill=tk.BOTH, expand=True, side=tk.LEFT)
+        self.tilesetFrame.pack(fill=tk.Y, side=tk.LEFT)
+        self.collisionFrame.pack(expand=True, fill=tk.Y, side=tk.LEFT)
+        self.config(menu=self.mainMenu)
+
+        self.resizable(False, True)
+        self.minsize(250, 800)
 
     def load_tileset(self, path):
-        self.tileset = Tileset.load(path)
+        print("Loading tileset from {}...".format(path))
+        self.tileset = TkTileset.load(path, PicklableOptions.READONLY)
         self.mapFrame.draw()
+        self.tilesetFrame.draw()
+        print("Tileset loaded.")
 
     def load_overworld(self, path):
+        print("Loading map from {}...".format(path))
         self.overworld = Overworld.load(path)
+        self.mapFrame.clear()
         self.mapFrame.draw()
+        self.mapFrame.radiobuttons.reset()
+        print("Map loaded.")
 
+    def save_overworld(self, path=None):
+        print("Saving map...")
+        self.overworld.dump(path)
+        print("Map saved.")
 
-if __name__ == '__main__':
-    App().run()
+    def close_overworld(self):
+        self.mapFrame.clear()
+        self.overworld = None
+
+    def run(self):
+        self.mainloop()
